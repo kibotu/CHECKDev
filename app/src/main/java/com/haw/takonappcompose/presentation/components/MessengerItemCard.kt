f31db2fc0e87df432df454e4db762147c2e47c85
Jan Rabe <jan.rabe@check24.de>
2024-05-03 14:16:41 +0200

    text color

--- app/src/main/java/com/haw/takonappcompose/presentation/components/MessengerItemCard.kt
+++ app/src/main/java/com/haw/takonappcompose/presentation/components/MessengerItemCard.kt
@@ -9,11 +9,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.haw.takonappcompose.ui.theme.BluePrimary
 import dev.jeziellago.compose.markdowntext.MarkdownText
+import okhttp3.internal.wait
 
 @Composable
 fun MessengerItemCard(
@@ -29,7 +31,8 @@ fun MessengerItemCard(
             modifier = Modifier
                 .wrapContentSize()
                 .padding(horizontal = 24.dp, vertical = 18.dp),
-            markdown = message
+            markdown = message,
+            style = TextStyle.Default.copy(color = Color.White)
         )
     }
 }
