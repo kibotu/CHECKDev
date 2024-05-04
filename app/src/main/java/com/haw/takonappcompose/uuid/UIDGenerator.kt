de25025d8f98438e63decddfa70a3fc3d7c275ca
Jan Rabe <jan.rabe@check24.de>
2024-05-04 09:34:10 +0200

    added uuid generator

---
+++ app/src/main/java/com/haw/takonappcompose/uuid/UIDGenerator.kt
@@ -0,0 +1,25 @@
+package com.haw.takonappcompose.uuid
+
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * Created by [Jan Rabe](https://kibotu.net).
+ */
+internal object UIDGenerator {
+
+    private const val START_UID = 0
+
+    /**
+     * https://winterbe.com/posts/2015/05/22/java8-concurrency-tutorial-atomic-concurrent-map-examples/
+     */
+    private val nextUID = AtomicInteger(START_UID)
+
+    fun newUID(): Int {
+        if (!isValid(nextUID.get())) {
+            error("UID pool depleted")
+        }
+        return nextUID.incrementAndGet()
+    }
+
+    private fun isValid(uid: Int): Boolean = uid >= START_UID
+}
